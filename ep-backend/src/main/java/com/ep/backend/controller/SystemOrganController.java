e8caf00bcc58a0bbae78193f6d4012faee612a8f
feicongcong <734820209@qq.com>
2018-01-31 11:36:44 +0800

    backEnd

--- ep-backend/src/main/java/com/ep/backend/controller/SystemOrganController.java
+++ ep-backend/src/main/java/com/ep/backend/controller/SystemOrganController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -66,5 +67,10 @@ public class SystemOrganController extends BackendController {
         return "/systemOrgan/index";
     }
 
+//    @GetMapping("createInit/{id}")
+//    public String index(Model model,@PathVariable
+//    ) {
+//
+//    }
 
 }
