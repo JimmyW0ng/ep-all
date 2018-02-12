3ec03e8bbb69abca5f184e192375e2fd74ca5e44
JW <32593055@qq.com>
2018-02-12 10:49:16 +0800

    课程评论

--- ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildBo.java
+++ ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildBo.java
@@ -1,6 +1,7 @@
 package com.ep.domain.pojo.bo;
 
 
+import com.ep.common.tool.DateTools;
 import com.ep.domain.pojo.po.EpMemberChildPo;
 import lombok.Data;
 
@@ -9,5 +10,9 @@ public class MemberChildBo extends EpMemberChildPo {
 
     private String avatar;
     private String sign;
+
+    public String getChildBirthdayFormat() {
+        return DateTools.dateToString(super.getChildBirthday(), DateTools.DATE_FMT_3);
+    }
 
 }
