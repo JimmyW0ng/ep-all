d8eb3d2bd4c5bef696738ed1594c72a35c9cba0b
JW <32593055@qq.com>
2018-02-27 17:37:13 +0800

    api代码更新

--- ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildClassBo.java
+++ ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildClassBo.java
@@ -2,6 +2,8 @@ package com.ep.domain.pojo.bo;
 
 import com.ep.common.tool.NumberTools;
 import com.ep.domain.pojo.po.EpOrderPo;
+import com.ep.domain.repository.domain.enums.EpOrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ public class MemberChildClassBo extends EpOrderPo {
     private Integer courseNum;
     private Integer honorNum;
     private Integer scheduleCommentNum;
+    @JsonIgnore
     private Boolean courseCommentFlag;
     private Integer lastCatelogIndex;
     private String label;
@@ -28,6 +31,18 @@ public class MemberChildClassBo extends EpOrderPo {
      */
     public String getPirzeFormat() {
         return NumberTools.getFormatPrice(super.getPrize());
+    }
+
+    /**
+     * 待评价课程标识
+     *
+     * @return
+     */
+    public Boolean getWaitCommentFlag() {
+        if (courseCommentFlag != null && !courseCommentFlag && super.getStatus().equals(EpOrderStatus.end)) {
+            return true;
+        }
+        return false;
     }
 
 }
