06ace8702acd5493d7c40078841d1fcbf97b72c7
wangyanji <wyj@tourongchangfu.com>
2018-03-21 16:39:25 +0800

    api代码更新

--- ep-domain/src/main/java/com/ep/domain/pojo/bo/OrganCourseBo.java
+++ ep-domain/src/main/java/com/ep/domain/pojo/bo/OrganCourseBo.java
@@ -1,6 +1,7 @@
 package com.ep.domain.pojo.bo;
 
 
+import com.ep.common.tool.DateTools;
 import com.ep.domain.pojo.po.EpOrganCoursePo;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,16 @@ public class OrganCourseBo extends EpOrganCoursePo {
     private String ognName;
     private String ognPhone;
     private String courseCatalogName;
+
+    public Long getEnterTimeStampStart() {
+        if (super.getEnterTimeStart() != null) {
+            return super.getEnterTimeStart().getTime();
+        }
+        return null;
+    }
+
+    public Long getServerTimeStamp() {
+        return DateTools.getCurrentDateTime().getTime();
+    }
 
 }
