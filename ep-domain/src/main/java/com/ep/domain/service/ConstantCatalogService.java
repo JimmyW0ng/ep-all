3fd6f3ed76e00438c53344b3f6b5413b284fedf1
feicongcong <734820209@qq.com>
2018-03-22 22:29:12 +0800

    backend

--- ep-domain/src/main/java/com/ep/domain/service/ConstantCatalogService.java
+++ ep-domain/src/main/java/com/ep/domain/service/ConstantCatalogService.java
@@ -1,19 +1,22 @@
 package com.ep.domain.service;
 
+import com.ep.domain.constant.BizConstant;
+import com.ep.domain.constant.MessageCode;
+import com.ep.domain.pojo.ResultDo;
 import com.ep.domain.pojo.po.EpConstantCatalogPo;
 import com.ep.domain.repository.ConstantCatalogRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.locks.Condition;
 
 /**
  * @Description:课程类目服务
  * @Author: CC.F
  * @Date: 21:59 2018/2/5
  */
+@Slf4j
 @Service
 public class ConstantCatalogService {
     @Autowired
@@ -23,20 +26,68 @@ public class ConstantCatalogService {
         return constantCatalogRepository.getAll();
     }
 
-    public EpConstantCatalogPo insert(EpConstantCatalogPo po) {
-        return constantCatalogRepository.create(po);
+    /**
+     * 新增产品类目
+     *
+     * @param po
+     * @return
+     */
+    public ResultDo createConstantCatalog(EpConstantCatalogPo po) {
+        log.info("[课程类目]，新增课程类目开始，po={}。", po);
+        if (po.getId() != null || po.getParentId() == null || po.getLabel() == null) {
+            log.error("[课程类目]，新增课程类目失败，接受参数异常。");
+            return ResultDo.build(MessageCode.ERROR_SYSTEM_PARAM_FORMAT);
+        }
+        constantCatalogRepository.insert(po);
+        log.info("[课程类目]，新增课程类目成功，id={}。", po.getId());
+        return ResultDo.build();
     }
 
-    public int update(EpConstantCatalogPo po) {
-        return constantCatalogRepository.updateConstantCatalogPo(po);
+    /**
+     * 修改产品类目
+     *
+     * @param po
+     * @return
+     */
+    public ResultDo updateConstantCatalog(EpConstantCatalogPo po) {
+        log.info("[课程类目]，修改课程类目开始，po={}。", po);
+        if (po.getId() == null || po.getParentId() == null || po.getLabel() == null) {
+            log.error("[课程类目]，修改课程类目失败，接受参数异常。");
+            return ResultDo.build(MessageCode.ERROR_SYSTEM_PARAM_FORMAT);
+        }
+        if (constantCatalogRepository.updateConstantCatalogPo(po) == BizConstant.DB_NUM_ONE) {
+            log.info("[课程类目]，修改课程类目成功，id={}。", po.getId());
+            return ResultDo.build();
+        } else {
+            log.error("[课程类目]，修改课程类目失败，id={}。", po.getId());
+            return ResultDo.build(MessageCode.ERROR_OPERATE_FAIL);
+        }
     }
 
+    /**
+     * 根据id获取记录
+     * @param id
+     * @return
+     */
     public EpConstantCatalogPo getById(Long id){
         return constantCatalogRepository.getById(id);
     }
 
-    public void delete(Long id){
-        constantCatalogRepository.deleteById(id);
+    /**
+     * 删除课程类目
+     *
+     * @param ids
+     * @return
+     */
+    public ResultDo delete(Long[] ids) {
+        log.info("[课程类目]，删除课程类目开始，ids={}。", ids);
+        if (constantCatalogRepository.deleteByIds(ids) == ids.length) {
+            log.info("[课程类目]，删除课程类目成功，ids={}。", ids);
+            return ResultDo.build();
+        } else {
+            log.info("[课程类目]，删除课程类目失败，ids={}。", ids);
+            return ResultDo.build(MessageCode.ERROR_OPERATE_FAIL);
+        }
     }
 
     public List<EpConstantCatalogPo> findSecondCatalog(){
