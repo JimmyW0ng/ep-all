21381a403a7402d5c361305fc299002c0766d26d
734820209@qq.com <fcc19921123>
2018-04-12 17:41:34 +0800

    backend

--- ep-backend/src/main/resources/static/modules/core.js
+++ ep-backend/src/main/resources/static/modules/core.js
@@ -250,6 +250,19 @@ function $ajaxPost(url, data, successfun) {
         }
     })
 }
+
+function apisMapClickCallback(success) {
+    window.addEventListener('message', function (event) {
+        // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
+        var loc = event.data;
+        if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
+            console.log('location', loc);
+            if (success) {
+                success(loc)
+            }
+        }
+    }, false);
+}
 $(function () {
     $("body").on("blur", "input.number-input", function () {
         if ($(this).val() == '') {
