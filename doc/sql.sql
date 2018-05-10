26f61f343c9550ad46bbe8da0347228cc94eb613
734820209@qq.com <fcc19921123>
2018-05-10 10:05:47 +0800

    backend bug修复

--- doc/sql.sql
+++ doc/sql.sql
@@ -16,3 +16,11 @@ ADD COLUMN `dura_type`  enum('minute','hour','day') NOT NULL DEFAULT 'minute' CO
 
 ALTER TABLE `ep_organ_class_schedule`
 ADD COLUMN `dura_type`  enum('minute','hour','day') NOT NULL DEFAULT 'minute' COMMENT '时长类型:分;时;天' AFTER `duration`;
+
+# ep_wechat_openid
+ALTER TABLE `ep_wechat_openid`
+ADD COLUMN `create_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' AFTER `mobile`,
+ADD COLUMN `update_at`  datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间' AFTER `create_at`,
+ADD COLUMN `remark`  varchar(255) NULL COMMENT '备注' AFTER `update_at`,
+ADD COLUMN `del_flag`  bit(1) NOT NULL DEFAULT b'0' COMMENT '删除标记' AFTER `remark`,
+ADD COLUMN `version`  bigint(20) NOT NULL DEFAULT 0 COMMENT '版本' AFTER `del_flag`;
