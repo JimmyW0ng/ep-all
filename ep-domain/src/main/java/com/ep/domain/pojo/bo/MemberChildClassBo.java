2cfe233f2c2f1839717be24a1884056c221cc3cb
wangyanji <wyj@tourongchangfu.com>
2018-05-14 09:30:50 +0800

    api代码更新

--- ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildClassBo.java
+++ ep-domain/src/main/java/com/ep/domain/pojo/bo/MemberChildClassBo.java
@@ -2,6 +2,7 @@ package com.ep.domain.pojo.bo;
 
 import com.ep.common.tool.NumberTools;
 import com.ep.domain.pojo.po.EpOrderPo;
+import com.ep.domain.repository.domain.enums.EpOrderPayType;
 import com.ep.domain.repository.domain.enums.EpOrderStatus;
 import com.ep.domain.repository.domain.enums.EpOrganCourseCourseType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -54,6 +55,22 @@ public class MemberChildClassBo extends EpOrderPo {
             return "课程";
         } else if (EpOrganCourseCourseType.activity.equals(this.getCourseType())) {
             return "活动";
+        }
+        return null;
+    }
+
+    /**
+     * 支付类型文案
+     *
+     * @return
+     */
+    public String getPayTypeFormat() {
+        if (super.getPayType() != null) {
+            if (EpOrderPayType.offline.equals(super.getPayType())) {
+                return "商户支付途径支付";
+            } else if (EpOrderPayType.wechat_pay.equals(super.getPayType())) {
+                return "平台在线支付";
+            }
         }
         return null;
     }
